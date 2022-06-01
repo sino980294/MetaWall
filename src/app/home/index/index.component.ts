bace8862400f3b469e32a7b74e4199cd6f00f2f1
Sino <wind1151@gmail.com>
2022-06-01 23:16:07 +0800

    刪除貼文

--- src/app/home/index/index.component.ts
+++ src/app/home/index/index.component.ts
@@ -2,8 +2,9 @@ import { PostDatapostsViewModel, UserInfo } from './../../app-info/typescript-an
 import { Posts_Service } from '../../app-info/typescript-angular-client-generated/typescript-angular-client/api/posts_.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { PostViewModel ,comment} from 'src/app/app-info/typescript-angular-client-generated/typescript-angular-client/model/models';
+import { PostViewModel, comment } from 'src/app/app-info/typescript-angular-client-generated/typescript-angular-client/model/models';
 import { faThumbsUp } from '@fortawesome/free-regular-svg-icons';
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 import { JwtTokenServiceService } from 'src/app/service/jwtTolenService.service';
 @Component({
   selector: 'app-index',
@@ -15,6 +16,7 @@ export class IndexComponent implements OnInit {
   public postMeaage: PostViewModel = new PostViewModel();
   public SearchPsotSetTimeOut!: ReturnType<typeof setTimeout>;
   public faThumbsUp = faThumbsUp;
+  public faEllipsisVertical = faEllipsisVertical;
   public Ilike = false;
   userInfo !: UserInfo;
 
@@ -42,7 +44,7 @@ export class IndexComponent implements OnInit {
       next: res => {
 
         this.postMeaage = res;
-        this.postMeaage.data.posts.map(x=>x.userCommentInput = "")
+        this.postMeaage.data.posts.map(x => x.userCommentInput = "")
         console.log(this.postMeaage)
       },
       error: err => {
@@ -72,19 +74,35 @@ export class IndexComponent implements OnInit {
     this.postService.postsIdCommentPost(posts.id, {
       "comment": posts.userCommentInput
     }).subscribe({
-      next:(res)=>{
+      next: (res) => {
         posts.userCommentInput = ""
-       let comments = new comment();
-       let resComment = res.data.comments;
-       comments.createAt = resComment.createAt
-       comments.comment = resComment.comment
-       comments.commentUser = this.userInfo
+        let comments = new comment();
+        let resComment = res.data.comments;
+        comments.createAt = resComment.createAt
+        comments.comment = resComment.comment
+        comments.commentUser = this.userInfo
         posts.comments.push(comments)
       },
-      error:(err)=>{
+      error: (err) => {
         alert(err.message)
       }
     })
+  }
+  deleteMessage(id: string) {
+    this.postService.postsIdDelete(id).subscribe({
+      next: (res) => {
+        if (res.status == 'success')
+          alert('刪除成功')
+        else
+          alert('刪除失敗')
+      },
+      error: err => {
+        alert('刪除失敗')
+      }
+    })
+  }
+  EditMessage(id: string){
+
   }
 }
 
